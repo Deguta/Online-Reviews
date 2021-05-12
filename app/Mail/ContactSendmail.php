fc12a274ad7e6bebf581b14bd4539ca20d67063f
Deguta <tech.ym1110@gmail.com>
2021-05-12 21:16:58 +0900

    MASTER UPDATE
    mailのコントローラーを作成

--- app/Mail/ContactSendmail.php
+++ app/Mail/ContactSendmail.php
@@ -1,24 +1,29 @@
 <?php
-
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ContactSendmail extends Mailable
 {
     use Queueable, SerializesModels;
+
+    private $email;
+    private $title;
+    private $body;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( $inputs )
     {
-        //
+        $this->email = $inputs['email'];
+        $this->title = $inputs['title'];
+        $this->body  = $inputs['body'];
     }
 
     /**
@@ -26,8 +31,17 @@ class ContactSendmail extends Mailable
      *
      * @return $this
      */
+
     public function build()
     {
-        return $this->view('view.name');
+        return $this
+            ->from('example@gmail.com')
+            ->subject('自動送信メール')
+            ->view('contacts.mail')
+            ->with([
+                'email' => $this->email,
+                'title' => $this->title,
+                'body'  => $this->body,
+            ]);
     }
 }
