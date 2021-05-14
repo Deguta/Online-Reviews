988676cd2d76ba62dfdf217a286fa3dd1d2cd62b
Deguta <tech.ym1110@gmail.com>
2021-05-14 23:56:59 +0900

    MASTER UPDATE
    ヘッダーを統一するためにviewを作成

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header class="navbar navbar-dark bg-primary ">
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand font-weight-bold" href="{{ url('/') }}">Study-Of-PostSite</a>
+        </div>
+</header>
+
