3596d48828466ad4e5df19ab0c2b35d68d995406
Deguta <tech.ym1110@gmail.com>
2021-05-31 23:53:06 +0900

    MASTER UPDATE
    ルーティング修正

--- resources/views/contacts/confirm.blade.php
+++ resources/views/contacts/confirm.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.bulletin-board-common')
+@extends('layouts.common')
 @section('title', 'お問い合わせ確認画面')
 <link rel="stylesheet" href="{{ asset('/css/bulletin-board/index-list.css') }}" >
 <link rel="stylesheet" href="{{ asset('/css/bulletin-board/index-list.css') }}" media="screen and (max-width:750px)" >
